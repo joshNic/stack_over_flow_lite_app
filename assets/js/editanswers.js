f615a8446708ebe1a243e86ebcf852e07a196922
joshNic <256jomu@gmail.com>
2018-09-19 17:17:36 +0300

    add update answer UI [Started #160620618]

--- assets/js/editanswers.js
+++ assets/js/editanswers.js
@@ -1,5 +1,5 @@
-
 let questionId = getUrlQuestionId()["question_id"];
+let answerId = getUrlQuestionId()["answer_id"];
 console.log(questionId);
 function getUrlQuestionId() {
     let urlVars = {};
@@ -8,34 +8,31 @@ function getUrlQuestionId() {
     });
     return urlVars;
 }
-
-url = 'http://127.0.0.1:5000/api/v2/question/'+questionId;
-answerUrl = 'http://127.0.0.1:5000/api/v2/question/'+questionId+'/answer';
+url = 'http://127.0.0.1:5000/api/v2/answer/' + answerId;
+url2 = 'http://127.0.0.1:5000/api/v2/question/'+questionId+'/answer/'+answerId;
 fetch(url)
     .then((res) => res.json())
     .then((data) => {
-        let output = `<h1>question</h1>
-                    <p>${data[0].question_title}?</p>
-                    <p>${data[0].question_body}?</p>
-                    <h1>Answers</h1>`;
-        data[0].answers.forEach(function (answer) {
-            output += `
-                    <hr>
-                    <p>${answer.answer_body}</p>
-                    <span><a href="#">Josh</a></span> <small>5 mins ago</small><small> Accepted: ${answer.answer_status}</small>
-                    <a href="editanswer.html?answer_id=${answer.answer_id}&question_id=${questionId}"><input type="submit" name="submit" value="edit"></a>
-                    `;
-        });
-        document.getElementById('answers').innerHTML = output;
+        let edit = `
+        <label for="qbody"><b>Update Your Answer</b></label>
+                <textarea rows="4" cols="50" id="answerBody">
+                    ${data[0].answer_body}
+                </textarea>
+                <div class="clearfix">
+                        <button type="submit" class="signupbtn">Update Answer</button>
+                </div>
+        `;
+        document.getElementById('editanswer').innerHTML = edit;
     })
 
-document.getElementById('postAnswer').addEventListener('submit', postAnswer);
 
-function postAnswer(e) {
+document.getElementById('editanswer').addEventListener('submit', editAnswer);
+
+function editAnswer(e) {
     e.preventDefault();
-    let answerBody = document.getElementById('answerBoby').value;
-    fetch(answerUrl, {
-        method: 'POST',
+    let answerBody = document.getElementById('answerBody').value;
+    fetch(url2, {
+        method: 'PUT',
         headers: {
             'Accept': 'application/json, text/plain, */*',
             'Content-type': 'application/json',
@@ -66,13 +63,10 @@ function postAnswer(e) {
                             <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
                             <strong>Yahh</strong> ${data.message}.
                         </div>`;
-                        
                         document.getElementById('message').innerHTML = message;
-                        setTimeout(function () { document.location.reload(true); }, 1000);
                     })
             }
         })
 
         .catch((error) => console.log(error))
 }
-
